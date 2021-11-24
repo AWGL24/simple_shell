f8b9d9b65b5be5729fc8e64e9c79d372c32e9660
Drew-24 <3239@holbertonschool.com>
2021-11-24 15:28:26 +0000

    our main file

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void)
+{
+	char userinput[MAXCOM];
+	char *parsedArgs[MAXLIST];
+	int execFlag = 0;
+
+	init_shell();
+
+	while (1)
+	{
+		print_line();
+
+		if (takeInput(userinput))
+			continue;
+
+		execFlag = processString(userinput, parsedArgs, parsedArgsPiped);
+
+		if (execFlag == 1)
+			execArgs(parsedArgs);
+
+		if (execFlag == 2)
+			execArgsPiped(parsedArgs, parsedArgsPiped);
+	}
+	return (0);
+}
