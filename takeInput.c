72f47dc543b93b078cc6f6b63dfdb34bdc65b9ba
Drew-24 <3239@holbertonschool.com>
2021-11-29 14:30:03 +0000

    function that takes input and pases it to the main file

---
+++ takeInput.c
@@ -0,0 +1,17 @@
+#include "main.h"
+int takeInput(char *str)
+{
+	char *buf;
+
+	buf = readline("\n>>> ");
+	if (strlen(buf) != 0)
+	{
+		add_history(buf);
+		strcpy(str, buf);
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
