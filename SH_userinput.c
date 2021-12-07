7c3f1c8596dda359b71c905422da786e70798b47
Drew-24 <andrewwil1@hotmail.com>
2021-12-07 17:45:02 +0000

    function that takes the user input and passes it thru getline for the computer to return the command that the user wants

---
+++ SH_userinput.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+char *userinput(void)
+{
+	size_t size = 0;
+	int index, dest = 0;
+	char *command, *buffer = NULL;
+
+	index = getline(&buffer, &size, stdin);
+
+	if (index < 0)
+	{
+		free(buffer);
+		exit(EXIT_FAILURE);
+	}
+
+	command = malloc(sizeof(char) * _strlen(buffer));
+
+	if (command == NULL)
+	{
+		free(command);
+		exit(EXIT_FAILURE);
+	}
+	_strcpy(command, buffer);
+	free(buffer);
+	return(command);
+}
