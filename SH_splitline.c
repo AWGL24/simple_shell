0ad757bb29c087bfbab66b6f2a201a67a517458d
Drew-24 <andrewwil1@hotmail.com>
2021-12-07 17:43:20 +0000

    function to split a string using a tokeniser function like strtok

---
+++ SH_splitline.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+#define SH_TOK_BUFSIZE 64
+#define SH_TOK_DELIM " \t\r\n\a"
+char **SH_splitline(char *line)
+{
+	int bufsize = SH_TOK_BUFSIZE, position = 0;
+	char **tokens;
+	char *token, **tokens_backup;
+
+	tokens = malloc(bufsize * sizeof(char *));
+
+	if (!tokens)
+	{
+		perror("SH: allocation error\n");
+		free(tokens);
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, SH_TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		token = strtok(NULL, SH_TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	free(tokens);
+	return (tokens);
+}
