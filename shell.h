b1918b7b06020e4b28d36900f7399ef5726da95a
Drew-24 <andrewwil1@hotmail.com>
2021-12-07 17:38:53 +0000

    SH header file with new prototypes nd libraries

--- shell.h
+++ shell.h
@@ -8,5 +8,23 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <signal.h>
-int main(int ac __attribute__((unused)), char **av __attribute__((unused)));
+typedef struct builtins {
+  char *cm;
+  char *dir;
+} builtin;
+extern char **environ;
+extern void free (void *__ptr) __THROW;
+char *userinput(void);
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int SH_exit(char **args);
+int SH_launch(char **args);
+int SH_execute(char **args);
+int env(void);
+char **SH_splitline(char *line);
+void _puts(char *str);
+int _putchar(char c);
+void safe_free(char **ptr);
+void memclean(char **array);
 #endif /* SHELL_H */
