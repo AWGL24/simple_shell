4293306adb28dc6de3f795337089578aa95f77fd
Drew-24 <andrewwil1@hotmail.com>
2021-12-07 17:37:30 +0000

    builted in function with our shell commands

---
+++ SH_builtin_functions.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+int _builtin_function(char **args)
+{
+	int idx = 0;
+	pid_t pid;
+	int status;
+
+	pid = fork();
+
+	builtin commands[] = {
+		{"ls", "/bin/ls"},
+		{"env", "/bin/env"},
+		{"cat", "/bin/cat"},
+		{NULL, NULL}
+	};
+
+	while (commands[idx].cm != NULL)
+	{
+		if (_strcmp(commands[idx].cm, args[0]) == 0)
+		{
+			if (pid == 0)
+			{
+				if (execve(commands[idx].dir, args, NULL) == -1)
+					perror("builtin");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid < 0)
+				perror("launch");
+			else
+				wait(&status);
+		}
+		idx++;
+	}
+	if (commands[idx].cm != NULL)
+		return (SH_launch(args));
+	return (0);
+}
