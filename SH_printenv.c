b9b0bb87c7d549074bd8ef2df2e7d62fcd36b4d8
Drew-24 <andrewwil1@hotmail.com>
2021-12-09 20:12:31 +0000

    fixed printenv function

--- SH_printenv.c
+++ SH_printenv.c
@@ -1,40 +1,17 @@
 #include "shell.h"
 /**
  * printenv - function that prints all the elements of the environment
- * @env: the environment
- * @PATH: the PATH stored in a 2darr
+ * Return: 0
  */
-void printenv(char **env, char **PATH)
+int printenv(void)
 {
-	int idx, idx2, idx3, idx4;
+	int count = 0;
 
-	for (idx = 0; env[idx] != NULL; idx++)
+	while (environ[count] != NULL)
 	{
-		if (_strncmp(env[idx], "PATH", 4) == 0)
-		{
-			for (idx3 = 0; env[idx][idx3] != '\0'; idx3++)
-			{
-				_putchar(env[idx][idx3]);
-			}
-			_putchar('=');
-			for (idx4 = 0; PATH[idx4] != NULL; idx4++)
-			{
-				for (idx3 = 0; PATH[idx4][idx3] != '\0'; idx3++)
-				{
-					_putchar(PATH[idx4][idx3]);
-				}
-				if (PATH[idx4 + 1] != NULL)
-					_putchar(':');
-			}
-			_putchar('\n');
-		}
-		else
-		{
-			for (idx2 = 0; env[idx][idx2] != '\0'; idx2++)
-			{
-				_putchar(env[idx][idx2]);
-			}
-			_putchar('\n');
-		}
+		write(STDOUT_FILENO, environ[count], _strlen(environ[count]));
+		write(STDOUT_FILENO, "\n", 1);
+		count++;
 	}
+	return (0);
 }
